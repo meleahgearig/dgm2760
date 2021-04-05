7362b5ac2d99214bd3a6f3549dcaa3c7e4c456fa
Meleah <meleahsanders@gmail.com>
2021-04-04 19:54:45 -0600

    needed more values

--- unit6/main.js
+++ unit6/main.js
@@ -11,14 +11,14 @@ function tellStory() {
 
     const myStory =
     `There once was a young <span>${nounArray[0]}</span> named Cinderella.
-    Her name was really Ruby but her stepsisters called her that because
+    Her name was really Ruby but her stepsisters <span>${verbArray[0]}ed</span> her that because
     she was always dirty. One day a <span>${adjectiveArray[0]}</span> invitation arrived
     at the house. The prince was going to have a royal <span>${nounArray[1]}</span>.
     Cinderella was not allowed to go. After the rest of the family was gone,
-    Cinderella's fairy <span>${nounArray[2]}</span> came to help. She used her magic and 
+    Cinderella's fairy <span>${nounArray[2]}</span> came to <span>${verbArray[1]}</span>. She used her magic and 
     Cinderella's rags turned into a <span>${adjectiveArray[1]}</span> dress. She had <span>${adjectiveArray[2]}</span>
     slippers and a coach made out of a <span>${nounArray[3]}</span>. She HAD to be home by midnight 
-    or the magic would <span>${verbArray[0]}</span>!`
+    or the magic would <span>${verbArray[2]}</span>!`
 
     document.querySelector('#story').innerHTML = myStory
 }
