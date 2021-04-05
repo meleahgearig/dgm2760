51cd5e2aabd9376d14b9e9d97d983bd6a1a2c81c
Meleah <meleahsanders@gmail.com>
2021-04-04 19:48:38 -0600

    finished up unit 6

---
+++ unit6/main.js
@@ -0,0 +1,24 @@
+function tellStory() {
+    nounString = document.querySelector('#noun').value
+    nounArray = nounString.toLowerCase().split(/\s+/)
+    console.log(nounArray)
+
+    adjectiveString = document.querySelector('#adjective').value
+    adjectiveArray = adjectiveString.toLowerCase().split(/\s+/)
+
+    verbString = document.querySelector('#verb').value
+    verbArray = verbString.toLowerCase().split(/\s+/)
+
+    const myStory =
+    `There once was a young <span>${nounArray[0]}</span> named Cinderella.
+    Her name was really Ruby but her stepsisters called her that because
+    she was always dirty. One day a <span>${adjectiveArray[0]}</span> invitation arrived
+    at the house. The prince was going to have a royal <span>${nounArray[1]}</span>.
+    Cinderella was not allowed to go. After the rest of the family was gone,
+    Cinderella's fairy <span>${nounArray[2]}</span> came to help. She used her magic and 
+    Cinderella's rags turned into a <span>${adjectiveArray[1]}</span> dress. She had <span>${adjectiveArray[2]}</span>
+    slippers and a coach made out of a <span>${nounArray[3]}</span>. She HAD to be home by midnight 
+    or the magic would <span>${verbArray[0]}</span>!`
+
+    document.querySelector('#story').innerHTML = myStory
+}
