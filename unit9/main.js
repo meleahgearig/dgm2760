1cf781a9949a1318095d613027228cad9444c242
Meleah <meleahsanders@gmail.com>
2021-04-18 23:32:04 -0600

    finished unit9

--- unit9/main.js
+++ unit9/main.js
@@ -5,14 +5,22 @@ function duplicateMenu() {
     //create the new list items for the bottom of the page
     let newList = document.createElement('ul')
 
+    
+
     topList.forEach(menuItem => {
         let newLI = document.createElement('li')
         let newLink = document.createElement('a')
         newLink.setAttribute('href', menuItem.getAttribute('href'))
-        newLI.textContent = menuItem.textContent
+        // 'copy' the same textContent from original to new
+        newLink.textContent = menuItem.textContent
         // append children to make them appear in the DOM
+        newLI.appendChild(newLink)
+        newList.appendChild(newLI)
         console.log(newLI)
     })
+    
+    document.querySelector('#smallNavArea').appendChild(newList)
+
 }
 
 duplicateMenu()
